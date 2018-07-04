b622e4522c1039c71d55fae26cc88555d6ef35f0
sandypants93 <sandypants93@gmail.com>
2018-07-04 09:11:41 -0400

    Modify LinkedList.c -finished defining basic functions

--- src/LinkedList.c
+++ src/LinkedList.c
@@ -12,6 +12,7 @@ void linkedListNodeInit(struct LinkedListNode *node, void *data)
     node = calloc(1, sizeof(struct LinkedListNode));
     if (node == NULL)
     {
+        perror("calloc");
         return;
     }
     node->data = data;
@@ -28,7 +29,7 @@ void linkedListInit(struct LinkedList *list, deleteFunction deleteNode)
     list = calloc(1, sizeof(struct LinkedList));
     if (list == NULL)
     {
-        perror("calloc")
+        perror("calloc");
         return;
     }
     linkedListNodeInit(list->sentry, NULL);
@@ -51,7 +52,6 @@ int linkedListPrepend(struct LinkedList *list, void *data)
      *************************************************************************/
     if (list == NULL)
     {
-        fprintf("linkedListPrepend: uninitialized LinkedList\n")
         return -1;
     }
     struct LinkedListNode *newNode;
@@ -80,8 +80,108 @@ int linkedListPrepend(struct LinkedList *list, void *data)
     return 0;
 }
 
-int linkedListAppend(struct LinkedList *list, void *data);
+int linkedListAppend(struct LinkedList *list, void *data)
+{
+    /**************************************************************************
+     * linkedListAppend will initialize a new node and place it at the end of
+     * list.
+     * 
+     * returns 0 on success and -1 on error.
+     *************************************************************************/
+    if (list == NULL)
+    {
+        return -1;
+    }
+    struct LinkedListNode *newNode;
+    linkedListNodeInit(newNode);
+    if (newNode == NULL)
+    {
+        return -1;
+    }
+    if (!(list->size))
+    {
+        list->sentry->next = newNode;
+        list->sentry->prev = newNode;
+        newNode->next = list->sentry;
+        newNode->prev = list->sentry;
+    }
+    else
+    {
+        struct LinkedListNode *oldTail;
+        oldTail = list->sentry->prev;
+        list->sentry->prev = newNode;
+        oldTail->next = newNode;
+        newNode->next = list->sentry;
+        newNode->prev = oldTail;
+    }
+    list->size += 1;
+    return 0;
+}
 
-void *linkedListPopHead(struct LinkedList *list);
+void *linkedListPopHead(struct LinkedList *list)
+{
+    /**************************************************************************
+     * linkedListPopHead will remove the head of the list, free it, and return
+     * a pointer to its data.
+     * 
+     * returns a pointer to the data in the head node on success and NULL on 
+     * error.
+     *************************************************************************/
+    if (list == NULL)
+    {
+        return NULL;           
+    }
+    struct LinkedListNode *newNode;
+    linkedListNodeInit(newNode);
+    if (newNode == NULL)
+    {
+        return NULL;
+    }
+    if (!(list->size))
+    {
+        return NULL;
+    }
+    struct LinkedListNode *oldHead, *newHead;
+    void *returnData;
+    oldHead = list->sentry->next;
+    newHead = oldHead->next;
+    list->sentry->next = newHead;
+    newHead->prev = list->sentry;
+    returnData = oldHead->data;
+    free(oldHead);
+    return returnData;
+}
 
-void *linkedListPopTail(struct LinkedList *list);
+void *linkedListPopTail(struct LinkedList *list)
+{
+    /**************************************************************************
+     * linkedListPopTail will remove the tail of the list, free it, and return
+     * a pointer to its data.
+     * 
+     * returns a pointer to the data in the tail node on success and NULL on 
+     * error.
+     *************************************************************************/
+    if (list == NULL)
+    {
+        return NULL;           
+    }
+    struct LinkedListNode *newNode;
+    linkedListNodeInit(newNode);
+    if (newNode == NULL)
+    {
+        return NULL;
+    }
+    if (!(list->size))
+    {
+        return NULL;
+    }
+    struct LinkedListNode *oldTail, *newTail;
+    void *returnData;
+    oldTail = list->sentry->prev;
+    newTail = oldTail->prev;
+    list->sentry->prev = newTail;
+    newTail->next = list->sentry;
+    returnData = oldTail->data;
+    free(oldTail);
+    return returnData;
+}
